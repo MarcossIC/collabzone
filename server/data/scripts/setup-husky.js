10311fb4105e28161f24d9bf710247e788b2f76b
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-11-05 15:00:44 -0300

    fix: .husky path

--- server/data/scripts/setup-husky.js
+++ server/data/scripts/setup-husky.js
@@ -4,7 +4,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.resolve(__filename, '../../..');
+const __dirname = path.resolve(path.dirname(__filename), '../..');
 
 try {
   // Initialize Husky
